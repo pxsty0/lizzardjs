baea5fd9e0955ddfe3341d6e0953287de8efa75d
Mustafa "pxsty" KÖK <75846829+pxsty0@users.noreply.github.com>
2024-06-01 20:43:26 +0300

    minor fixes 2

--- src/functions/process.rs
+++ src/functions/process.rs
@@ -5,10 +5,10 @@ mod os;
 pub fn init_process(scope: &mut ContextScope<HandleScope>, global: v8::Local<v8::Object>) {
     let process = v8::ObjectTemplate::new(scope);
 
-    os::init_os(scope, process_obj);
-
     let process_key = v8::String::new(scope, "process").unwrap();
     let process_obj = process.new_instance(scope).unwrap();
+
+    os::init_os(scope, process_obj);
 
     global.set(scope, process_key.into(), process_obj.into());
 }
